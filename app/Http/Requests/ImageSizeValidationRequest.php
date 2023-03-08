af99470c8d57b4e28d6c765b320408f2e3148fb0
Dharmendra <dharmendra@avdevs.com>
2023-03-08 18:58:19 +0530

    code changes

--- app/Http/Requests/ImageSizeValidationRequest.php
+++ app/Http/Requests/ImageSizeValidationRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ImageSizeValidationRequest extends FormRequest
-{
+{ 
     /**
      * Determine if the user is authorized to make this request.
      */
